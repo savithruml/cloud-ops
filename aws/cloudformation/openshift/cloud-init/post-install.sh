69c2f2e33e045637b9344767dae71baef3935129
Savithru Lokanath <savithruml@users.noreply.github.com>
2018-05-02 00:28:00 -0700

    Update post-install.sh

--- aws/cloudformation/openshift/cloud-init/post-install.sh
+++ aws/cloudformation/openshift/cloud-init/post-install.sh
@@ -1,5 +1,2 @@
 CONTAINER=$(docker ps | grep -i kube-man | cut -d " " -f1 | head -1)
 docker restart "$CONTAINER"
-ssh root@${MinionIPv4Address} iptables -I INPUT 4 -j ACCEPT
-oadm policy add-cluster-role-to-user cluster-admin admin
-htpasswd -bc /etc/origin/master/htpasswd admin contrail123
